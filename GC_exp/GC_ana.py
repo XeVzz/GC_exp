ab6a6ad0e1d0c4d7176c7b1483329302d0768b11
MinLunWu <j6aup3xjp6@gmail.com>
2022-11-30 14:05:53 +0800

    GC  new_ver

--- GC_exp/GC_ana.py
+++ GC_exp/GC_ana.py
@@ -52,9 +52,10 @@ def df_plot(data):
     loc,scale=norm.fit(data)
     n = norm(loc=loc, scale=scale)
     x = np.linspace(np.min(data), np.max(data), len(data))
-
     # x = np.arange(np.min(data), np.max(data), res)
+    
     plt.plot(x, n.cdf(x),'r-.',label='norm. CDF')
+    
     # k-s test
     # stat,p_val=ks_2samp(x, n.cdf(x))
     stat,p_val=kstest(data, 'norm',args=(data.mean(),data.std()))
@@ -83,7 +84,7 @@ def df_plot(data):
     return ax
 
 def scat_plot(x,y,colormsk=[]):
-    x=x[~np.isnan(x)]; x_std=np.std(x); x_mean=np.mean(x)
+    x=x[~np.isnan(x)]; x_std=np.std(x); x_mean=np.mean(x);
     y=y[~np.isnan(y)]; y_std=np.std(y); y_mean=np.mean(y)
     
     fig,ax=plt.subplots(1,1,figsize=(15,13))
@@ -92,29 +93,24 @@ def scat_plot(x,y,colormsk=[]):
     
     if len(np.unique(colormsk))==0:
         plt.plot(x_mean/x_std,y_mean/y_std,'*',c=ct(0),markersize=12,label='mean')  
-        
         cir1=plt.Circle((x_mean/x_std, y_mean/y_std), 1, facecolor='none',edgecolor=ct(1),linewidth=3,label='std range')
         cir2=plt.Circle((x_mean/x_std, y_mean/y_std), 2, facecolor='none',edgecolor=ct(2),linewidth=3,label='std*2 range')
         cir3=plt.Circle((x_mean/x_std, y_mean/y_std), 3, facecolor='none',edgecolor=ct(3),linewidth=3,label='std*3 range')
         ax.add_patch(cir1); ax.add_patch(cir2); ax.add_patch(cir3)
-        
         plt.scatter(x/x_std, y/y_std, facecolors='none', edgecolors='k',alpha=0.7,s=50)
 
     else :
         plt.plot(x_mean/x_std,y_mean/y_std,'*',c='k',markersize=12,label='mean')
-        
         cir1=plt.Circle((x_mean/x_std, y_mean/y_std), 1, facecolor='none',edgecolor='k',alpha=0.8,linewidth=1.5,label='std range')
         cir2=plt.Circle((x_mean/x_std, y_mean/y_std), 2, facecolor='none',edgecolor='k',linestyle='dashed',linewidth=1.5,label='std*2 range')
         cir3=plt.Circle((x_mean/x_std, y_mean/y_std), 3, facecolor='none',edgecolor='k',linestyle='dotted',linewidth=2.3,label='std*3 range')
         ax.add_patch(cir1); ax.add_patch(cir2); ax.add_patch(cir3)
-
         for color in np.unique(colormsk):
             plt.scatter(x[colormsk==color]/x_std, y[colormsk==color]/y_std, facecolors='none', edgecolors=color,alpha=0.5,s=40)
-            
-            
+                 
     ax.set_aspect('equal', 'box')
-    ax.set_ylim(-10, 10)
-    ax.set_xlim(-10, 10)
+    ax.set_ylim(floor(y_mean/y_std)-10, ceil(y_mean/y_std)+10)
+    ax.set_xlim(floor(x_mean/x_std)-10, ceil(x_mean/x_std)+10)
     
     plt.grid()
     ax.legend(fontsize=20) 
@@ -125,27 +121,13 @@ def scat_plot(x,y,colormsk=[]):
 
 #%%  file path & list setting
 ST_path='./ST_gc_no files/'
-
 ST_files = [_ for _ in os.listdir(ST_path) if _.endswith(".csv")]
-# VS_files = [_ for _ in os.listdir(VS_path) if _.endswith(".txt")]
-
-st_vs_cc=0; gc_cc=0; gc_len=500
 
 gc_log=dict()
-new_gc_log=dict()
-diff=dict()
-
-no_launch=0
 for key in ['dP', 'dT', 'dRH', 'num']:
     gc_log[key]=np.full(len(ST_files),np.nan)
-    new_gc_log[key]=np.full(len(ST_files),np.nan)
-    diff[key]=np.full(len(ST_files),np.nan)
-
-mon_idx=np.full(len(ST_files),np.nan)
-time_idx=np.full(len(ST_files),np.nan)
 
 ori_clmns=['Time','Pressure(hPa)','Temperature(degree C)','Humidity(%)']
-
 base_data=dict()
 
 base_data[0]=pd.DataFrame(pd.read_csv(ST_path+ST_files[0]))
@@ -162,53 +144,24 @@ base_data[1][ori_clmns[1]]=base_data[1][ori_clmns[1]]-2.2678
 base_data[1][ori_clmns[2]]=base_data[1][ori_clmns[2]]-1.6153
 base_data[1][ori_clmns[3]]=base_data[1][ori_clmns[3]]-3.6843
 
+base=pd.concat([base_data[0],base_data[1]]).sort_values(by='Time').reset_index(drop=True)
+# base_dt=base['Time'].apply(lambda x: datetime.strptime(x,'%Y/%m/%d %H:%M:%S.%f'))
+
 # %% file loop
 for i in np.arange(2,len(ST_files)):
-# for i in [5]:
-
-# channel identify
-    fname=ST_files[i].split('_')
-    channel=int(fname[1][-1])
+# for i in [4]:
     
-    if channel <5 :
-        base=base_data[0]
-        base_dt=base['Time'].apply(lambda x: datetime.strptime(x,'%Y/%m/%d %H:%M:%S.%f'))
-
-    elif channel>=5 :
-        base=base_data[1]
-        base_dt=base['Time'].apply(lambda x: datetime.strptime(x,'%Y/%m/%d %H:%M:%S.%f'))
-
 #  ST_file
     target_data=[]
     target_data=pd.DataFrame(pd.read_csv(ST_path+ST_files[i]))
     target_data.columns=target_data.columns.str.strip()
     target_data=target_data.sort_values(by='Time').reset_index(drop=True)
-    target_dt=target_data['Time'].apply(lambda x: datetime.strptime(x,'%Y/%m/%d %H:%M:%S.%f'))
+    # target_dt=target_data['Time'].apply(lambda x: datetime.strptime(x,'%Y/%m/%d %H:%M:%S.%f'))
 
-##find corresponding data
-    crpd_num=np.full([len(target_data)],np.nan)
-    crpd_data=pd.DataFrame()
-    for num in range(len(target_data)):
-        tmp=base_dt[(base_dt==target_dt[num])].index.tolist()
-        if len(tmp) >=1:    
-            crpd_num[num]=tmp[0]
-            crpd_data=crpd_data.append(base.iloc[tmp[0],:], ignore_index=True)
-        else:
-            continue
+# find corresponding data
+    crpd_tag=base['Time'].isin(target_data['Time'])
+    crpd_data=base[crpd_tag]
 
-    if len(crpd_data) <=120:
-        base=base_data[1]
-        base_dt=base['Time'].apply(lambda x: datetime.strptime(x,'%Y/%m/%d %H:%M:%S.%f'))
-        crpd_num=np.full([len(target_data)],np.nan)
-        crpd_data=pd.DataFrame()
-        for num in range(len(target_data)):
-            tmp=base_dt[(base_dt==target_dt[num])].index.tolist()
-            if len(tmp) >=1:    
-                crpd_num[num]=tmp[0]
-                crpd_data=crpd_data.append(base.iloc[tmp[0],:], ignore_index=True)
-            else:
-                continue
-            
     if len(crpd_data) <=120:
         print(ST_files[i]+' has no corresponding data')
         continue
@@ -216,44 +169,33 @@ for i in np.arange(2,len(ST_files)):
     crpd_data=crpd_data.loc[:,ori_clmns].reset_index(drop=True)
     crpd_data.columns=['datetime','P','T','RH']
 
-##
+# target data generate
     P=target_data['Pressure(hPa)']
     T=target_data['Temperature(degree C)']
     RH=target_data['Humidity(%)']
     
-    valid= (P>=0) & (P<1050) & (T!=-46.85) & (T!=40.99) & (RH>=0) & ~np.isnan(crpd_num)
+    valid= (P>=0) & (P<1050) & (T!=-46.85) & (T!=40.99) & (RH>=0) & target_data['Time'].isin(base['Time'])
     target=target_data.loc[valid,ori_clmns].reset_index(drop=True)
     target.columns=['datetime','P','T','RH']
 
-# gc
+# gc data prepare
     if len(target)<630:
         first=1-1; last=len(target)-1
     else:
         first=len(target)-10-600-1; last=len(target)-10-1
-        
+
     gc_data=dict()
-    
     gc_data['P_st']=target['P'][first:last].values
     gc_data['T_st']=target['T'][first:last].values
     gc_data['RH_st']=target['RH'][first:last].values
 
-##
-    gc_log['num'][i]=len(gc_data['P_st'])
-
     gc_data['P_obs']=crpd_data['P'][first:last].values
     gc_data['T_obs']=crpd_data['T'][first:last].values
     gc_data['RH_obs']=crpd_data['RH'][first:last].values
-
-##
+    
+    gc_log['num'][i]=len(gc_data['P_st'])
     gc_log['dP'][i],gc_log['dT'][i],gc_log['dRH'][i]=gc.bias(gc_data['P_st'],gc_data['T_st'],gc_data['RH_st'],
                                                              gc_data['P_obs'],gc_data['T_obs'],gc_data['RH_obs'])
-
-    # if gc_log['dP'][i]>0.5:
-        # gc_log['dP'][i]=0
-    # if abs(gc_log['dT'][i])<=0.3:
-        # gc_log['dP'][i]=0
-    # if abs(gc_log['dRH'][i])<=2:
-        # gc_log['dRH'][i]=0
 
 # %% CDF & PDF
 ax=df_plot(gc_log['dT'])
@@ -272,15 +214,17 @@ plt.title('dRH distribution',fontsize=20)
 plt.savefig('dRH_dist', dpi=500) 
 
 # %% scatter color as batch
-colormsk=np.append(np.full(116,'b'),np.full(154,'r'))
-colormsk=colormsk[~np.isnan(gc_log['dT'])]
+# colormsk=np.concatenate((np.full(116,'b'), np.full(154,'r')))
+# colormsk=colormsk[~np.isnan(gc_log['dT'])]
 
 # %% dT-dRH scatter
-ax,par=scat_plot(gc_log['dT'],gc_log['dRH'],colormsk)
-plt.text(-9.7, -7.3, 'dT_std = '+str(round(par[0][0],3))+' $^\circ$C',ha='left', size=16,weight='bold')
-plt.text(-9.7, -8, 'dRH_std = '+str(round(par[0][1],3))+' %', ha='left', size=16,weight='bold')
-plt.text(-9.7, -9, 'dT_mean = '+str(round(par[1][0],3))+' $^\circ$C', ha='left', size=16,weight='bold',color='r')
-plt.text(-9.7, -9.7, 'dRH_mean = '+str(round(par[1][1],3))+' %', ha='left', size=16,weight='bold',color='r')
+ax,par=scat_plot(gc_log['dT'],gc_log['dRH'])
+xlim=par[1][0]/par[0][0]-10
+ylim=par[1][1]/par[0][1]-10
+plt.text(xlim+0.3, ylim+2.7, 'dT_std = '+str(round(par[0][0],3))+' $^\circ$C',ha='left', size=16,weight='bold')
+plt.text(xlim+0.3, ylim+2, 'dRH_std = '+str(round(par[0][1],3))+' %', ha='left', size=16,weight='bold')
+plt.text(xlim+0.3, ylim+1, 'dT_mean = '+str(round(par[1][0],3))+' $^\circ$C', ha='left', size=16,weight='bold',color='r')
+plt.text(xlim+0.3, ylim+0.3, 'dRH_mean = '+str(round(par[1][1],3))+' %', ha='left', size=16,weight='bold',color='r')
 ax.set_xlabel('dT / T_std',fontsize=24)
 ax.set_ylabel('dRH / RH_std',fontsize=24)
 plt.title('dT-dRH scatter',fontsize=30)
@@ -288,51 +232,14 @@ plt.savefig('dT-dRH_scatter', dpi=300)
 
 # %% dP-dT scatter
 ax,par=scat_plot(gc_log['dP'],gc_log['dT'])
-plt.text(-9.7, -7.3, 'dP_std = '+str(round(par[0][0],3))+' hPa', ha='left', size=16,weight='bold')
-plt.text(-9.7, -8, 'dT_std = '+str(round(par[0][1],3))+' $^\circ$C', ha='left', size=16,weight='bold')
-plt.text(-9.7, -9, 'dP_mean = '+str(round(par[1][0],3))+' hPa', ha='left', size=16,weight='bold',color='r')
-plt.text(-9.7, -9.7, 'dT_mean = '+str(round(par[1][1],3))+' $^\circ$C', ha='left', size=16,weight='bold',color='r')
+xlim=par[1][0]/par[0][0]-10
+ylim=par[1][1]/par[0][1]-10
+plt.text(xlim+0.3, ylim+2.7, 'dP_std = '+str(round(par[0][0],3))+' hPa', ha='left', size=16,weight='bold')
+plt.text(xlim+0.3, ylim+2, 'dT_std = '+str(round(par[0][1],3))+' $^\circ$C', ha='left', size=16,weight='bold')
+plt.text(xlim+0.3, ylim+1, 'dP_mean = '+str(round(par[1][0],3))+' hPa', ha='left', size=16,weight='bold',color='r')
+plt.text(xlim+0.3, ylim+0.3, 'dT_mean = '+str(round(par[1][1],3))+' $^\circ$C', ha='left', size=16,weight='bold',color='r')
 ax.set_xlabel('dP / P_std',fontsize=24)
 ax.set_ylabel('dT / T_std',fontsize=24)
 plt.title('dP-dT scatter',fontsize=30)
 plt.savefig('dP-dT_scatter', dpi=300) 
-
-# %% K-S test
-data=gc_log['dT']
-data=data[~np.isnan(data)]
-a=np.sort(data[~np.isnan(data)])    
-p = 1. * np.arange(len(a)) / float(len(a) - 1)  
-
-loc,scale=norm.fit(data)
-n = norm(loc=loc, scale=scale)
-x = np.linspace(np.min(data), np.max(data), len(data))
-norm_p=n.cdf(x)
-DD=n.cdf(x)-p
-D=max(abs(DD))
-critical=1.36/np.sqrt(len(data))
-a=kstest(data, 'norm',args=(data.mean(),data.std()))
-critical=1.36/np.sqrt(len(data))
-
-aa=normaltest(data,nan_policy='omit')
-
-# %%
-# data=gc_log['dRH']
-# data=data[~np.isnan(data)]
-# ks_dRH=kstest(data, 'norm')
-# loc,scale=norm.fit(data)
-# n = norm(loc=loc, scale=scale)
-# x = np.arange(np.min(data), np.max(data), 0.2)
-
-# # stat,p_val=ks_2samp(x, n.cdf(x))
-
-# # %%
-# data=gc_log['dP']
-# data=data[~np.isnan(data)]
-# ks_dP=kstest(data, 'norm')
-# loc,scale=norm.fit(data)
-# n = norm(loc=loc, scale=scale)
-# x = np.arange(np.min(data), np.max(data), 0.2)
-# # stat,p_val=ks_2samp(x, n.cdf(x))
-
-
 
